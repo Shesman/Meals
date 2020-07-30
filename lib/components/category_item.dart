687053efadb88c6950186e60ebb0dfac52e7f6f0
ShesmanRenato <shesmanrenato@MacSrsg.local>
2020-07-30 13:23:59 -0300

    Add de Telas de Refeicoes por categoria

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/category.dart';
-import '../screens/categories_meals_screen.dart';
+import '../utils/app_routes.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
@@ -11,12 +11,9 @@ class CategoryItem extends StatelessWidget {
   );
 
   void _selectCategory(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (_) {
-          return CategoriesMealsScreen(category);
-        },
-      ),
+    Navigator.of(context).pushNamed(
+      AppRoutes.CATEGORIES_MEALS,
+      arguments: category,
     );
   }
 
